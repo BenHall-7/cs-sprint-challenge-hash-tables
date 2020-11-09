418e5a4407fdc3aa3793c56e86127af781c99246
BenHall-7 <benjaminjahall@gmail.com>
2020-11-08 21:30:18 -0500

    Do example 1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,11 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    prev = {} # maps weight to index
+    for i in range(length):
+        cur = weights[i]
+        diff = limit - cur
+        if diff in prev:
+            return sorted((i, prev[diff]), reverse=True)
+        else:
+            prev[cur] = i
 
     return None
