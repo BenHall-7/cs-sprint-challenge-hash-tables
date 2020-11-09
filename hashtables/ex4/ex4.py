ae1303ba3f81e35dc9c4f3f46d654b7b6cf80245
BenHall-7 <benjaminjahall@gmail.com>
2020-11-08 23:05:19 -0500

    Do example 4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,11 +1,12 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    data = {}
+    for num in a:
+        if -num in data:
+            data[-num] = True
+        elif num not in data:
+            data[num] = False
 
-    return result
-
+    return list(map(lambda i: abs(i[0]), filter(lambda i: i[1], data.items())))
 
 if __name__ == "__main__":
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
