7a3cff70e70f8405389b79ab2c548b2282fb1a47
BenHall-7 <benjaminjahall@gmail.com>
2020-11-08 23:25:10 -0500

    Finish all exercises

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,12 +1,17 @@
-# Your code here
-
-
-
 def finder(files, queries):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    file_map = {}
+    for full in files:
+        ind = full.rindex('/')
+        filename = full if ind < 0 else full[ind + 1:]
+        if filename in file_map:
+            file_map[filename].append(full)
+        else:
+            file_map[filename] = [full]
+
+    result = []
+    for q in queries:
+        if q in file_map:
+            result.extend(file_map[q])
 
     return result
 
